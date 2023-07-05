b75b0d58ffaeebbd73dcabb4a97711e83b4db535
Muria <muriafreedom14@gmail.com>
2023-07-05 07:42:45 +0700

    update

--- resources/views/layouts/kandidat.blade.php
+++ resources/views/layouts/kandidat.blade.php
@@ -4,7 +4,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <title>DIGIJOB-UGIPORT</title>
         <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
-        <link rel="icon" href="/Atlantis/examples/assets/img/icon.ico" type="image/x-icon"/>
+        <link rel="icon" href="/gambar/icon.ico" type="image/x-icon"/>
         <link rel="stylesheet" href="/moving.css">
         <style>
             .bold{
