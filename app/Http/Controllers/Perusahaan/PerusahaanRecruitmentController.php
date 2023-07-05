98a823f6026317064662d19ce1d85e8bc1ca70f5
FREEDOM <muriafreedom14@gmail.com>
2023-07-06 02:51:53 +0700

    update

--- app/Http/Controllers/Perusahaan/PerusahaanRecruitmentController.php
+++ app/Http/Controllers/Perusahaan/PerusahaanRecruitmentController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Perusahaan;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\perusahaan;
+use App\Models\Perusahaan;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Negara;
 use App\Models\PerusahaanNegara;
