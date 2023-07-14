b94397641ca1864f5cd61939360e3b3f934abff0
Muria <muriafreedom14@gmail.com>
2023-07-14 15:24:06 +0700

    update

--- app/Http/Controllers/Kandidat/KandidatController.php
+++ app/Http/Controllers/Kandidat/KandidatController.php
@@ -51,17 +51,17 @@ class KandidatController extends Controller
         } else {
             $perusahaan = null;
         }
-        $persetujuan = Perusahaan::join(
-            'persetujuan_kandidat', 'persetujuan_kandidat.id_perusahaan','=','perusahaan.id_perusahaan'
-        )
-        ->where('persetujuan_kandidat.nama_kandidat',$kandidat->nama)->where('persetujuan_kandidat.id_kandidat',$kandidat->id_kandidat)->first();
-        if($persetujuan->persetujuan == null){
-            $persetujuan = $persetujuan;
-        } else {
-            $persetujuan = null;
-        }
+        // $persetujuan = Perusahaan::join(
+        //     'persetujuan_kandidat', 'persetujuan_kandidat.id_perusahaan','=','perusahaan.id_perusahaan'
+        // )
+        // ->where('persetujuan_kandidat.nama_kandidat',$kandidat->nama)->where('persetujuan_kandidat.id_kandidat',$kandidat->id_kandidat)->first();
+        // if($persetujuan->persetujuan == null){
+        //     $persetujuan = $persetujuan;
+        // } else {
+        //     $persetujuan = null;
+        // }
         return view('kandidat/index',compact('kandidat','notif','perusahaan_semua',
-        'perusahaan','pembayaran','pesan','lowongan','cari_perusahaan','persetujuan'));
+        'perusahaan','pembayaran','pesan','lowongan','cari_perusahaan'));
     }
     
     public function profil()
