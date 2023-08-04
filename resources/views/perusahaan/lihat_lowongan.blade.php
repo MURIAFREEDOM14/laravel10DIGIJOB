a9ae71c889f5516e70b14c9d31712d8a86289256
FREEDOM <muriafreedom14@gmail.com>
2023-08-04 21:05:26 +0700

    update

--- resources/views/perusahaan/lihat_lowongan.blade.php
+++ resources/views/perusahaan/lihat_lowongan.blade.php
@@ -171,10 +171,20 @@
                         <div class=""><b class="bold">: {{($lowongan->negara)}}</b></div>
                     </div>
                 </div>
+                <hr>
+                <div  class="row">
+                    <div class="col-md-3">
+                        <label for="" class="">Judul Pekerjaan</label>
+                    </div>
+                    <div class="col-md-8">
+                        <div class=""><b class="bold">: {{($lowongan->jabatan)}}</b></div>
+                    </div>
+                </div>
+                <hr>
                 @if ($lowongan->lvl_pekerjaan !== null)
                     <div  class="row">
                         <div class="col-md-3">
-                            <label for="" class="">Tingkatan Pekerja</label>
+                            <label for="" class="">Jenis Pekerjaan</label>
                         </div>
                         <div class="col-md-8">
                             <div class=""><b class="bold">: {{($lowongan->lvl_pekerjaan)}}</b></div>
@@ -182,15 +192,6 @@
                     </div>
                     <hr>    
                 @endif
-                <div  class="row">
-                    <div class="col-md-3">
-                        <label for="" class="">Spesifikasi Pekerjaan</label>
-                    </div>
-                    <div class="col-md-8">
-                        <div class=""><b class="bold">: {{($lowongan->jabatan)}}</b></div>
-                    </div>
-                </div>
-                <hr>
                 @if ($lowongan->isi !== null)
                     <div  class="row">
                         <div class="col-md-3">
@@ -202,6 +203,101 @@
                     </div>
                     <hr>    
                 @endif
+                @if ($lowongan->gambar_lowongan !== null)
+                    <div class="row">
+                        <div class="col-md-3">
+                            <label for="">Gambar</label>
+                        </div>
+                        <div class="col-md-4">
+                            <img src="/gambar/Perusahaan/{{$perusahaan->nama_perusahaan}}/Lowongan Pekerjaan/{{$lowongan->gambar_lowongan}}" width="250" height="250" alt="" class="img">                            
+                        </div>
+                    </div>    
+                    <hr>
+                @endif
+                <hr>
+                <div  class="row">
+                    <div class="col-md-3">
+                        <h5><b class="bold">Persyaratan</b></h5>
+                    </div>
+                </div>
+                <hr>
+                <div  class="row">
+                    <div class="col-md-3">
+                        <label for="" class="">Jenis Kelamin</label>
+                    </div>
+                    <div class="col-md-8">
+                        <div class=""><b class="bold">: 
+                            @if ($lowongan->jenis_kelamin == "M")
+                                Laki-laki    
+                            @elseif($lowongan->jenis_kelamin == "F")
+                                Perempuan
+                            @else
+                                Laki-laki & Perempuan
+                            @endif
+                        </b></div>
+                    </div>
+                </div>
+                <hr>
+                <div  class="row">
+                    <div class="col-md-3">
+                        <label for="" class="">Pendidikan</label>
+                    </div>
+                    <div class="col-md-8">
+                        <div class=""><b class="bold">: {{$lowongan->pendidikan}}</b></div>
+                    </div>
+                </div>
+                <hr>
+                @if ($lowongan->usia !== null)
+                    <div  class="row">
+                        <div class="col-md-3">
+                            <label for="" class="">Usia</label>
+                        </div>
+                        <div class="col-md-8">
+                            <div class=""><b class="bold">: {{$lowongan->usia}} Sampai {{$lowongan->usia}}</b></div>
+                        </div>
+                    </div>
+                    <hr>    
+                @endif
+                <div  class="row">
+                    <div class="col-md-3">
+                        <label for="" class="">Pengalaman Kerja</label>
+                    </div>
+                    <div class="col-md-8">
+                        <div class=""><b class="bold">: 
+                            @if ($lowongan->pengalaman_kerja == null)
+                                Tidak ada batasan
+                            @else    
+                                {{$lowongan->pengalaman_kerja}}
+                            @endif
+                        </b></div>
+                    </div>
+                </div>
+                <hr>
+                @if ($lowongan->tinggi !== null)
+                    <div class="row">
+                        <div class="col-md-3">
+                            <label for="">Syarat Tinggi Badan Minimal</label>
+                        </div>
+                        <div class="col-md-3">
+                            <b class="bold">: {{$lowongan->tinggi}}</b>
+                        </div>
+                    </div>
+                @endif
+                <div  class="row">
+                    <div class="col-md-3">
+                        <label for="" class="">Syarat Berat Badan minimal</label>
+                    </div>
+                    <div class="col-md-3">
+                        <div class=""><b class="bold">: 
+                            @if ($lowongan->berat == null)
+                                Tidak ada batasan
+                            @else
+                                {{$lowongan->berat}}
+                            @endif
+                        </b></div>
+                    </div>
+                </div>
+                <hr>
                 @if ($lowongan->mata_uang !== null)
                     <div  class="row">
                         <div class="col-md-3">
@@ -238,17 +334,7 @@
                     <hr>    
                 @endif
                 
-                @if ($lowongan->gambar_lowongan !== null)
-                    <hr>
-                    <div class="row">
-                        <div class="col-md-3">
-                            <label for="">Gambar</label>
-                        </div>
-                        <div class="col-md-4">
-                            <img src="/gambar/Perusahaan/{{$perusahaan->nama_perusahaan}}/Lowongan Pekerjaan/{{$lowongan->gambar_lowongan}}" width="250" height="250" alt="" class="img">                            
-                        </div>
-                    </div>    
-                @endif
+                
                 <hr>
                 @if ($lowongan->ttp_lowongan !== null)
                     <div class="row">
@@ -280,94 +366,12 @@
                         <div class=""><b class="bold">: {{$perusahaan->referral_code}}</b></div>
                     </div>
                 </div>
-                <hr>
-                <div  class="row">
-                    <div class="col-md-3">
-                        <h5><b class="bold">Persyaratan</b></h5>
-                    </div>
-                </div>
-                <hr>
-                <div  class="row">
-                    <div class="col-md-3">
-                        <label for="" class="">Jenis Kelamin</label>
-                    </div>
-                    <div class="col-md-8">
-                        <div class=""><b class="bold">: 
-                            @if ($lowongan->jenis_kelamin == "M")
-                                Laki-laki    
-                            @elseif($lowongan->jenis_kelamin == "F")
-                                Perempuan
-                            @else
-                                Laki-laki & Perempuan
-                            @endif
-                        </b></div>
-                    </div>
-                </div>
-                <hr>
-                <div  class="row">
-                    <div class="col-md-3">
-                        <label for="" class="">Pendidikan</label>
-                    </div>
-                    <div class="col-md-8">
-                        <div class=""><b class="bold">: {{$lowongan->pendidikan}}</b></div>
-                    </div>
-                </div>
-                <hr>
-                <div  class="row">
-                    <div class="col-md-3">
-                        <label for="" class="">Usia</label>
-                    </div>
-                    <div class="col-md-8">
-                        <div class=""><b class="bold">: 
-                            @if ($lowongan->usia == null)
-                                Tidak ada batasan
-                            @else    
-                                {{$lowongan->usia}}
-                            @endif
-                        </b></div>
-                    </div>
-                </div>
-                <hr>
-                <div  class="row">
-                    <div class="col-md-3">
-                        <label for="" class="">Pengalaman Kerja</label>
-                    </div>
-                    <div class="col-md-8">
-                        <div class=""><b class="bold">: 
-                            @if ($lowongan->pengalaman_kerja == null)
-                                Tidak ada batasan
-                            @else    
-                                {{$lowongan->pengalaman_kerja}}
-                            @endif
-                        </b></div>
-                    </div>
-                </div>
-                <hr>
-                <div  class="row">
-                    <div class="col-md-3">
-                        <label for="" class="">Berat</label>
-                    </div>
-                    <div class="col-md-3">
-                        <div class=""><b class="bold">: 
-                            @if ($lowongan->berat == null)
-                                Tidak ada batasan
-                            @else
-                                {{$lowongan->berat}}
-                            @endif
-                        </b></div>
-                    </div>
-                    <div class="col-md-3">
-                        <label for="">Tinggi</label>
-                    </div>
-                    <div class="col-md-3"><b class="bold">: 
-                        @if ($lowongan->tinggi == null)
-                            Tidak ada batasan
-                        @else
-                            {{$lowongan->tinggi}}
-                        @endif
-                    </b></div>
-                </div>
-                <hr>
+                
+                
+                
+                
+                
+                
                 <div  class="row">
                     <div class="col-md-3">
                         <label for="" class="">Lokasi Pencarian Kandidat</label>
