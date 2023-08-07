6e77e12693d360efafd7283698fb2bc51a6789a8
FREEDOM <muriafreedom14@gmail.com>
2023-08-08 05:01:04 +0700

    update

--- resources/views/auth/register_semua.blade.php
+++ resources/views/auth/register_semua.blade.php
@@ -35,7 +35,7 @@
             <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="exampleModalLabel">Tutorial Daftar</h1>
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Tutorial Daftar Pencari Kerja / Kandidat</h1>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                         <div class="modal-body text-center">
