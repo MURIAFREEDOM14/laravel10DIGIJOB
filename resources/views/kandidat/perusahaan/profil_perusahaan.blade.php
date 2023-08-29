cbe4d8cb9a3ef03940d663a38c0a940636d31754
Muria <muriafreedom14@gmail.com>
2023-08-29 14:47:53 +0700

    update

--- resources/views/kandidat/perusahaan/profil_perusahaan.blade.php
+++ resources/views/kandidat/perusahaan/profil_perusahaan.blade.php
@@ -40,7 +40,15 @@
                         @endif
                     </div>
                 </div>
-                @if ($kandidat->id_perusahaan == $perusahaan->id_perusahaan && $kandidat->stat_pemilik == "diambil")
+                @if($kandidat->id_perusahaan == $perusahaan->id_perusahaan || $kandidat->id_perusahaan !== null)
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="">Permohonan Lowongan sudah dikirimkan</div>
+                            <hr>
+                            <a href="/keluar_perusahaan/{{$perusahaan->id_perusahaan}}" class="btn btn-outline-danger mx-auto" onclick="cancelLowongan(event)">Batalkan Lowongan</a>
+                        </div>
+                    </div>
+                @elseif ($kandidat->id_perusahaan == $perusahaan->id_perusahaan && $kandidat->stat_pemilik == "diambil")
                     <div class="card">
                         <div class="card-body">
                             <div class="">Permohonan Lowongan sudah dikirimkan</div>
