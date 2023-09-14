0040dc07acb9c995ed348225bff60ea73f38d2b6
Muria <muriafreedom14@gmail.com>
2023-09-14 16:49:45 +0700

    update

--- resources/views/layouts/input.blade.php
+++ resources/views/layouts/input.blade.php
@@ -98,7 +98,7 @@
           @yield('content')
         </div>
       </main>
-      <button style="">Contact Us</button>
+      {{-- <button style="">Contact Us</button> --}}
       <div class="loading align">
         <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
       </div>
