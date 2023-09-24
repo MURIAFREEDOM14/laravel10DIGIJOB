43686f40c8c68868c23bdb0ff7c40a5ea60721fb
MURIAFREEDOM14 <muriafreedom14@gmail.com>
2023-09-25 06:47:10 +0700

    update

--- resources/views/features.blade.php
+++ resources/views/features.blade.php
@@ -8,16 +8,10 @@
             <div class="card-body">
                 <ul>
                     <li>
-                        Jaminan kehadiran pekerja di Pusat Medis Terakreditasi untuk pemeriksaan kesehatan pekerja.
-                    </li>
-                    <li>
                         Total statistik pekerja tersedia secara online.
                     </li>
                     <li>
                         Memungkinkan penugasan pekerja ke calon pemberi kerja.
-                    </li>
-                    <li>
-                        Terintegrasi penuh dengan sistem Bio-Medis FWCMS.
                     </li>
                     <li>
                         Layanan berbasis cloud sepenuhnya - berbiaya rendah, tidak perlu instalasi.
